b70eb057918c7cab956438e9c0cd9e9b7ff35317
Kidong Lee <mykidong@gmail.com>
2016-08-03 16:34:56 +0900

    .

--- src/main/java/kafka/etl/KafkaETLParquetConsumer.java
+++ src/main/java/kafka/etl/KafkaETLParquetConsumer.java
@@ -2,6 +2,7 @@ package kafka.etl;
 
 
 import kafka.etl.deserialize.AvroDeserializeService;
+import kafka.etl.deserialize.ClasspathAvroDeserializeService;
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.hadoop.conf.Configuration;
@@ -41,6 +42,47 @@ public class KafkaETLParquetConsumer {
     private AvroDeserializeService avroDeserializeService;
 
     private KafkaConsumer<Integer, byte[]> consumer;
+
+    public static void main(String[] args)
+    {
+        String brokers = System.getProperty("brokers", "localhost:9092");
+
+        // kafka consumer properties.
+        Properties kafkaConsumerProps = new Properties();
+        kafkaConsumerProps.put("bootstrap.servers", brokers);
+        kafkaConsumerProps.put("group.id", "kafka-etl-test-group");
+        kafkaConsumerProps.put("enable.auto.commit", "false");
+        kafkaConsumerProps.put("session.timeout.ms", "30000");
+        kafkaConsumerProps.put("key.deserializer", "org.apache.kafka.common.serialization.IntegerDeserializer");
+        kafkaConsumerProps.put("value.deserializer", "org.apache.kafka.common.serialization.ByteArrayDeserializer");
+
+        // topic list which should consume.
+        List<String> topics = new ArrayList<>();
+        topics.add("item-view-event");
+
+        String output = "/data/kafka-etl-parquet-test";
+
+        // kafka consumer poll timeout.
+        long pollTimeout = 1000;
+
+        // parquet sink related properties.
+        Map<String,String> parquetProps = new HashMap<>();
+        parquetProps.put(KafkaETLParquetConsumer.CONF_HADOOP_CONF_DIR, "/etc/hadoop/conf");
+        parquetProps.put(KafkaETLParquetConsumer.CONF_OUTPUT, output);
+        parquetProps.put(KafkaETLParquetConsumer.CONF_DATE_FORMAT, "yyyy-MM-dd/HH/mm");
+        parquetProps.put(KafkaETLParquetConsumer.CONF_INTERVAL_UNIT, KafkaETLParquetConsumer.IntervalUnit.MINUTE.name());
+        parquetProps.put(KafkaETLParquetConsumer.CONF_INTERVAL, "1");
+
+        // topic and avro schema classpath properties.
+        // topic key must be in topic list.
+        Properties topicAndPathProps = new Properties();
+        topicAndPathProps.put("item-view-event", "/META-INF/avro/item-view-event.avsc");
+
+
+        KafkaETLParquetConsumer kafkaETLConsumer =
+                new KafkaETLParquetConsumer(kafkaConsumerProps, topics, pollTimeout, parquetProps, new ClasspathAvroDeserializeService(topicAndPathProps));
+        kafkaETLConsumer.run();
+    }
 
 
     public static enum IntervalUnit {
--- src/test/java/kafka/etl/KafkaETLConsumerMain.java
+++ src/test/java/kafka/etl/KafkaETLConsumerMain.java
@@ -16,49 +16,11 @@ public class KafkaETLConsumerMain {
 
     public static void main(String[] args)
     {
-        java.net.URL url = new KafkaETLParquetConsumerTestSkip().getClass().getResource("/log4j-test.xml");
+        java.net.URL url = new KafkaETLConsumerMain().getClass().getResource("/log4j-test.xml");
         System.out.println("log4j url: " + url.toString());
 
         DOMConfigurator.configure(url);
 
-        log = LoggerFactory.getLogger(KafkaETLConsumerMain.class);
-
-        String brokers = System.getProperty("brokers", "localhost:9092");
-
-        // kafka consumer properties.
-        Properties kafkaConsumerProps = new Properties();
-        kafkaConsumerProps.put("bootstrap.servers", brokers);
-        kafkaConsumerProps.put("group.id", "kafka-etl-test-group");
-        kafkaConsumerProps.put("enable.auto.commit", "false");
-        kafkaConsumerProps.put("session.timeout.ms", "30000");
-        kafkaConsumerProps.put("key.deserializer", "org.apache.kafka.common.serialization.IntegerDeserializer");
-        kafkaConsumerProps.put("value.deserializer", "org.apache.kafka.common.serialization.ByteArrayDeserializer");
-
-        // topic list which should consume.
-        List<String> topics = new ArrayList<>();
-        topics.add("item-view-event");
-
-        String output = "/data/kafka-etl-parquet-test";
-
-        // kafka consumer poll timeout.
-        long pollTimeout = 1000;
-
-        // parquet sink related properties.
-        Map<String,String> parquetProps = new HashMap<>();
-        parquetProps.put(KafkaETLParquetConsumer.CONF_HADOOP_CONF_DIR, "/etc/hadoop/conf");
-        parquetProps.put(KafkaETLParquetConsumer.CONF_OUTPUT, output);
-        parquetProps.put(KafkaETLParquetConsumer.CONF_DATE_FORMAT, "yyyy-MM-dd/HH/mm");
-        parquetProps.put(KafkaETLParquetConsumer.CONF_INTERVAL_UNIT, KafkaETLParquetConsumer.IntervalUnit.MINUTE.name());
-        parquetProps.put(KafkaETLParquetConsumer.CONF_INTERVAL, "1");
-
-        // topic and avro schema classpath properties.
-        // topic key must be in topic list.
-        Properties topicAndPathProps = new Properties();
-        topicAndPathProps.put("item-view-event", "/META-INF/avro/item-view-event.avsc");
-
-
-        KafkaETLParquetConsumer kafkaETLConsumer =
-                new KafkaETLParquetConsumer(kafkaConsumerProps, topics, pollTimeout, parquetProps, new ClasspathAvroDeserializeService(topicAndPathProps));
-        kafkaETLConsumer.run();
+        KafkaETLParquetConsumer.main(args);
     }
 }
